f1c2b68422840911596076e016ea87c40c4e7f4d
Tyler Norbury <tylernorbury96@gmail.com>
2021-01-09 16:50:49 -0800

    Starting point for tutorial

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_receive_shared_data_tutorial/share_service.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Receiving Shared Text',
+      home: MyHomePage(title: 'Shared Text'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _sharedText = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create the share service
+    ShareService()
+      // Register a callback so that we handle shared data if it arrives when the
+      // app is running
+      ..onDataReceived = _handleSharedData
+
+      // Check to see if there is any shared data already, meaning that the app
+      // was launch via sharing.
+      ..getSharedData().then(_handleSharedData);
+  }
+
+  /// Handles any shared data we may receive.
+  void _handleSharedData(String sharedData) {
+    setState(() {
+      _sharedText = sharedData;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'The shared text that you received is:',
+            ),
+            Text(_sharedText)
+          ],
+        ),
+      ),
+    );
+  }
+}
