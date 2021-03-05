33174fcc87a7c460645524f5e435203a3258da72
Tyler Norbury <tylernorbury96@gmail.com>
2021-03-04 18:58:40 -0800

    upgrade to Flutter 2 & null safety

--- lib/share_service.dart
+++ lib/share_service.dart
@@ -3,13 +3,13 @@ import 'package:flutter/services.dart';
 /// This service is responsible for talking with the OS to see if anything was
 /// shared with the application.
 class ShareService {
-  void Function(String) onDataReceived;
+  void Function(String)? onDataReceived;
 
   ShareService() {
     // If sharing causes the app to be resumed, we'll check to see if we got any
     // shared data
-    SystemChannels.lifecycle.setMessageHandler((msg) {
-      if (msg.contains("resumed")) {
+    SystemChannels.lifecycle.setMessageHandler((msg) async {
+      if (msg?.contains("resumed") ?? false) {
         getSharedData().then((String data) {
           // Nothing was shared with us :(
           if (data.isEmpty) {
@@ -17,7 +17,7 @@ class ShareService {
           }
 
           // We got something! Inform our listener.
-          onDataReceived(data);
+          onDataReceived?.call(data);
         });
       }
       return;
